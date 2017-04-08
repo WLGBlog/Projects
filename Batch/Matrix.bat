88e79935de089c7581332a7b5f3c9776b011653a
WLG Blog <wlgblogemail@gmail.com>
2017-04-08 11:37:06 -0400

    Create Matrix.bat

---
+++ Batch/Matrix.bat
@@ -0,0 +1,6 @@
+@echo off
+title Matrix
+color 02
+:matrix
+echo %Random% %Random% %Random% %Random% %Random% %Random% %Random% %Random% %Random% %Random% %Random% %Random% %Random% %Random% %Random% %Random% %Random% %Random% %Random% %Random% %Random%
+goto matrix
