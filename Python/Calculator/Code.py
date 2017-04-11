3a7022f2724df7aec6ba52087a7b6cd18aff457e
WLG Blog <wlgblogemail@gmail.com>
2017-04-11 13:25:48 -0400

    Create Code.py

---
+++ Python/Calculator/Code.py
@@ -0,0 +1,26 @@
+print('1. Addition \n2. Subtraction \n3. Multiplication \n4. Division\n\n')
+operator = input('Which would you like to do? \n')
+
+if operator == '1':
+  num1=int(input('Enter First Number: '))
+  num2=int(input('Enter Second Number: '))
+  result=num1+num2
+  print(num1,'+',num2,"=",result)
+  
+if operator == '2':
+  num1=int(input('Enter First Number: '))
+  num2=int(input('Enter Second Number: '))
+  result=num1-num2
+  print(num1,'-',num2,"=",result)
+  
+if operator == '3':
+  num1=int(input('Enter First Number: '))
+  num2=int(input('Enter Second Number: '))
+  result=num1*num2
+  print(num1,'*',num2,"=",result)
+  
+if operator == '4':
+  num1=int(input('Enter First Number: '))
+  num2=int(input('Enter Second Number: '))
+  result=num1/num2
+  print(num1,'/',num2,"=",result)
